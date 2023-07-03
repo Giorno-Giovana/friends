8e1922ceea10ca79ded2d3a64e80477a88128ca3
ebolblga <kkochanovskiy@gmail.com>
2023-07-03 04:20:58 +0300

    fix:all events are displayed,event color from seed

--- pages/room/[id].vue
+++ pages/room/[id].vue
@@ -16,7 +16,10 @@ interface IEvent {
   end: Date,
   title: string,
   class: string,
-  background: boolean
+  background: boolean,
+  deletable: boolean,
+  resizable: boolean,
+  draggable: boolean
 };
 
 onMounted(async () => {
@@ -29,7 +32,7 @@ onMounted(async () => {
 
 const addEvent = (newEvent: any) => {
   room.value.events.push({ start: newEvent.start, end: newEvent.end, title: username.value });
-  events.value.push({ start: newEvent.start, end: newEvent.end, title: username.value, class: 'event', background: false });
+  events.value.push({ start: newEvent.start, end: newEvent.end, title: username.value, class: 'event', background: false, deletable: true, resizable: true, draggable: true });
   const response = $fetch<IRoom>('/api/room', { method: 'PUT', body: { ...room.value } });
   console.log(response);
   findMeetingTime();
@@ -72,27 +75,29 @@ async function findMeetingTime() {
     }
   });
 
-  if (!flag) events.value.push({ title: 'Встреча', start: bestRange.start, end: bestRange.end, class: 'meeting', background: true });
+  if (!flag) events.value.push({ title: 'Встреча-', start: bestRange.start, end: bestRange.end, class: 'meeting', background: true, deletable: false, resizable: false, draggable: false });
 }
 
 async function updateDisplayedEvents() {
   room.value.events.forEach( item => {
     if (item.title === username.value) {
-      events.value.push({title: item.title, start: new Date(item.start), end: new Date(item.end), class: 'event', background: false});
+      events.value.push({title: item.title, start: new Date(item.start), end: new Date(item.end), class: 'event', background: false, deletable: true, resizable: true, draggable: true});
+    } else {
+      events.value.push({title: item.title, start: new Date(item.start), end: new Date(item.end), class: getColorClass(item.title), background: true, deletable: false, resizable: false, draggable: false});
     }
   });
 
   findMeetingTime();
 }
 
-// async function roomNameChange(e) {
-//   if (!e.target) return;
-//   const newName = <HTMLInputElement>(e.target).innerText;
-//   room.value.title = newName.toString();
-//   useHead({ title: `${room.value.title}` });
-//   const response = await $fetch<IRoom>('/api/room', { method: 'PUT', body: { ...room.value, title: room.value.title } });
-//   console.log(response);
-// }
+function getColorClass(str: string) {
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    sum += str.charCodeAt(i);
+  }
+
+  return '_' + sum % 10;
+}
 
 async function copyRoute() {
   navigator.clipboard.writeText(window.location.href);
@@ -103,8 +108,7 @@ async function copyRoute() {
       <NuxtLink to="/" class="absolute left-1 top-2">
         <Icon class="hover:scale-95" name="ic:baseline-arrow-back-ios-new" size="32px" color="#EC81FE" />
       </NuxtLink>
-      <div class="h-[17vh] w-[90vw] lg:w-[50vw] min-w-[300px] mx-auto">
-        <!-- <p class="text-center">Комната "<span contenteditable class="cursor-pointer" @input="roomNameChange($event)">{{ room.title }}</span>"</p> -->
+      <div class="h-[19vh] w-[81vw] lg:w-[50vw] min-w-[300px] mx-auto">
         <p class="text-center w-full inline">Комната: </p>
         <input v-model="room.title" class="outline-none bg-transparent cursor-pointer w-[70%]" />
         <my-input id="name" placeholder="Введите имя" v-model="username">Ваше имя</my-input>
@@ -112,14 +116,12 @@ async function copyRoute() {
           <Icon class="ml-3" name="ic:baseline-content-copy" size="24px" />
         </my-button>
       </div>
-      <div class="h-[83vh]">
+      <div class="h-[81vh] p-2">
         <vue-cal
         ref="vuecal"
           small
           :disable-views="['years', 'year', 'month', 'day']"
           hide-view-selector
-          :time-from="0 * 60"
-          :time-to="24 * 60"
           :time-step="60"
           :snap-to-time="15"
           locale="ru"
@@ -131,18 +133,47 @@ async function copyRoute() {
     </div>
 </template>
 <style>
+/*Ивенты с классом meeting*/
 .vuecal__event.meeting {
-  background: repeating-linear-gradient(45deg, transparent, transparent 10px, #f2f2f2 10px, #f2f2f2 20px);/* IE 10+ */
-  color: #EC81FE;
+  background: repeating-linear-gradient(45deg, transparent, transparent 10px, #8d8d8d 10px, #8d8d8d 20px);/* IE 10+ */
+  color: #FFFFFF;
   font-weight: 700;
   display: flex;
   justify-content: center;
   align-items: center;
+  text-shadow: 2px 2px 1px black;
 }
 
+/*Ивенты с классом event*/
 .vuecal__event.event {
   background: rgba(236, 129, 254, 0.5);
   color: #000000;
-  font-weight: 400;
+  font-weight: 500;
 }
+
+.vuecal__cell--today, .vuecal__cell--current {background-color: rgba(0, 0, 0, 0.25);} /*Цвет подсветки сегодняшнего дня*/
+.vuecal__cell--selected {background-color: rgba(0, 0, 0, 0.5);} /*Цвет подсветки выделенного дня*/
+.vuecal__now-line {color: #EC81FE;} /*Цвет линии времени*/
+
+.vuecal__event._0 {background-color: rgba(223, 7, 114, 0.2);}
+.vuecal__event._1 {background-color: rgba(254, 84, 111, 0.2);}
+.vuecal__event._2 {background-color: rgba(255, 158, 125, 0.2);}
+.vuecal__event._3 {background-color: rgba(255, 208, 128, 0.2);}
+.vuecal__event._4 {background-color: rgba(255, 253, 255, 0.2);}
+.vuecal__event._5 {background-color: rgba(11, 255, 230, 0.2);}
+.vuecal__event._6 {background-color: rgba(1, 203, 207, 0.2);}
+.vuecal__event._7 {background-color: rgba(1, 136, 165, 0.2);}
+.vuecal__event._8 {background-color: rgba(62, 50, 100, 0.2);}
+.vuecal__event._9 {background-color: rgba(53, 42, 85, 0.2);}
+
+/* .vuecal__event._0 {background-color: rgba(251, 248, 253, 0.2);}
+.vuecal__event._1 {background-color: rgba(161, 169, 209, 0.2);}
+.vuecal__event._2 {background-color: rgba(0, 127, 255, 0.2);}
+.vuecal__event._3 {background-color: rgba(36, 37, 111, 0.2);}
+.vuecal__event._4 {background-color: rgba(20, 18, 24, 0.2);}
+.vuecal__event._5 {background-color: rgba(95, 14, 82, 0.2);}
+.vuecal__event._6 {background-color: rgba(253, 26, 67, 0.2);}
+.vuecal__event._7 {background-color: rgba(255, 177, 108, 0.2);}
+.vuecal__event._8 {background-color: rgba(254, 222, 91, 0.2);}
+.vuecal__event._9 {background-color: rgba(116, 234, 214, 0.2);} */
 </style>
